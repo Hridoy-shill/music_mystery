e682d0e3ef3b1f5ac4137065fb48be2ec7f90a8d
Hridoy-shill <ridoyaaa60@gmail.com>
2023-06-15 17:47:42 +0600

    my enrolled class fuctionality added

--- src/Pages/StudentPanel/MyEnrolledClasses.jsx
+++ src/Pages/StudentPanel/MyEnrolledClasses.jsx
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../Provider/AuthProvider';
+import Tittle from '../../Common_Component\'s/Tittle';
 
 const MyEnrolledClasses = () => {
+    const [myClasses, setMyClasses] = useState([]);
+    console.log(myClasses);
+
+    const { user } = useContext(AuthContext);
+    const { email } = user || {}
+
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/myEnrolledClasses/${email}`)
+            .then(res => res.json())
+            .then(data => setMyClasses(data))
+    }, [user])
+
     return (
-        <div>
-            enrolled classes
+        <div className='w-full'>
+            <Tittle heading={"My Enrolled Classes"}></Tittle>
+            <div className="overflow-x-auto w-10/12 mx-auto">
+                <table className="table mt-8">
+                    {/* head */}
+                    <thead>
+                        <tr className='text-center'>
+                            <th className='font-bold text-black text-base border-2 border-black hover:bg-teal-500 duration-700'>Course Image</th>
+                            <th className='font-bold text-black text-base border-2 border-black hover:bg-teal-500 duration-700'>Course Name</th>
+                            <th className='font-bold text-black text-base border-2 border-black hover:bg-teal-500 duration-700'>Teacher Name</th>
+                            <th className='font-bold text-black text-base border-2 border-black hover:bg-teal-500 duration-700'>Payed</th>
+                            <th className='font-bold text-black text-base border-2 border-black hover:bg-teal-500 duration-700'>Id</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            myClasses.map(enrolledClass => <tr key={enrolledClass._id} enrolledClass={enrolledClass}>
+                                <td className='font-bold text-black text-base border-2 border-black hover:bg-teal-500 duration-700 text-center'>
+                                    <div className="flex items-center space-x-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-circle w-16 h-1w-16">
+                                                <img src={enrolledClass.courseImg}/>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td className='font-bold text-black text-base border-2 border-black hover:bg-teal-500 duration-700 text-center'>{enrolledClass.courseName}</td>
+                                <td className='font-bold text-black text-base border-2 border-black hover:bg-teal-500 duration-700 text-center'>{enrolledClass.teacherName}</td>
+                                <td className='font-bold text-black text-base border-2 border-black hover:bg-teal-500 duration-700 text-center'>$ {enrolledClass.Price}</td>
+                                <td className='font-bold text-black text-base border-2 border-black hover:bg-teal-500 duration-700 text-center'>{enrolledClass.transactionId}</td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
