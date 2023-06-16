2440ee53d8fe6740a6d8c66663460452efa4c66b
Hridoy-shill <ridoyaaa60@gmail.com>
2023-06-16 22:21:28 +0600

    admination added

--- src/Pages/Home/HomeComponents/BannerSection/Banner.jsx
+++ src/Pages/Home/HomeComponents/BannerSection/Banner.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
+import { Fade, Slide } from "react-awesome-reveal";
 import './Banner.css'
 
 const Banner = () => {
@@ -10,17 +11,22 @@ const Banner = () => {
                 <img className='relative z-10' src="https://i.ibb.co/hWK3Lfg/8-Easy-Guitar-Songs-For-Every-Beginner-2.png" />
                 <div className='absolute top-0 bg-gradient-to-r from-teal-300  z-20 w-full h-full opacity-60'></div>
                 <div className='absolute text-2xl font-bold text-white z-40 top-56 left-20 w-2/4'>
-                    <h3 className='text-start font-bold text-5xl text-yellow-300'>Learn Flute..!</h3>
-                    <p className='text-start mt-5 font-thin text-3xl hover:text-yellow-300 duration-700'>In whispers of breeze, its melody sings,
-                        A slender companion, the flute takes wing.
-                        Crafted with care, a vessel of art,
-                        It breathes life into notes, touching the heart.</p>
+                    <Slide>
+                        <h3 className='text-start font-bold text-5xl text-yellow-300'>Learn Flute..!</h3>
+                    </Slide>
+                    <Fade delay={1e3} cascade damping={1e-1}>
+                        <p className='text-start mt-5 font-thin text-3xl hover:text-yellow-300 duration-700'>In whispers of breeze, its melody sings,
+                            A slender companion, the flute takes wing.
+                            Crafted with care, a vessel of art,
+                            It breathes life into notes, touching the heart.
+                        </p>
+                    </Fade>
                     <div className='text-start mt-5'>
                         <button className='p-2 btn btn-outline border-2 hover:border-yellow-300 hover:text-white text-white border-white hover:bg-transparent duration-500'>Learn more</button>
                     </div>
                 </div>
             </div>
-            
+
             <div>
                 <img className='relative z-10' src="https://i.ibb.co/KqHQFNQ/8-Easy-Guitar-Songs-For-Every-Beginner-3.png" />
                 <div className='absolute top-0 bg-gradient-to-r from-teal-300  z-20 w-full h-full opacity-60'></div>
